6ecf0a49e3aa7ec8d82c8119f822f21e3ae475c5
unknown <thffhahs03@gmail.com>
2025-08-28 19:12:49 +0900

    mesh 실습 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import ThreeElement from "./components/ThreeElement";
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 function App() {
+  const { color, gridSize, segment } = useControls({
+    color: "green",
+    gridSize: { value: 15, min: 1, max: 100, step: 1 },
+    segment: { value: 20, min: 2, max: 100, step: 1 },
+  });
   return (
     <>
-      <Canvas>
+      <Canvas camera={{ fov: 80, near: 1, far: 1000, position: [5, 5, 5] }}>
+        <color attach="background" args={[color]} />
         <ThreeElement />
+
+        <OrbitControls
+          // minAzimuthAngle={-Math.PI / 4}
+          // maxAzimuthAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 4}
+          maxPolarAngle={Math.PI / 1}
+        />
+        {/* 카메라 조작 컨트롤러 */}
+
+        <axesHelper args={[5]} />
+        {/* args = 축의 길이 */}
+
+        <gridHelper args={[gridSize, segment, "red", "black"]} />
+        {/* args = [그리드 한 칸의 크기, 그리드의 칸 수, 그리드 색상, 축 색상] */}
       </Canvas>
     </>
   );
--- src/components/ThreeElement.tsx
+++ src/components/ThreeElement.tsx
@@ -1,26 +1,49 @@
 import { useFrame, useThree } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { useControls } from "leva";
+import { useRef } from "react";
 import * as THREE from "three";
+import { Box } from "@react-three/drei";
 
 const ThreeElement = () => {
   const boxMeshRef = useRef<THREE.Mesh>(null);
-  const { size, gl, scene } = useThree();
-
+  // const { size, gl, scene } = useThree();
+  const { rotationX, x, y, z } = useControls({
+    rotationX: { value: 0, min: -360, max: 360, step: 0.3 },
+    x: { value: 0, min: -10, max: 10, step: 0.1 },
+    y: { value: 0, min: -10, max: 10, step: 0.1 },
+    z: { value: 0, min: -10, max: 10, step: 0.1 },
+  });
+  const { clock } = useThree();
   useFrame((state, delta) => {
     // 박스 회전
     if (boxMeshRef.current) {
-      boxMeshRef.current.rotation.y += delta;
+      boxMeshRef.current.rotation.y += Math.sin(delta);
+      boxMeshRef.current.position.x = Math.cos(clock.getElapsedTime()) * 3;
+      boxMeshRef.current.position.z = Math.sin(clock.getElapsedTime()) * 3;
     }
   });
-
-  console.log("render");
   return (
     <>
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
-      <mesh rotation={[Math.PI / 4, Math.PI / 4, 0]} ref={boxMeshRef}>
-        <boxGeometry />
-        <meshStandardMaterial color={"blue"} />
-      </mesh>
+      <group position={[3, 0, 0]}>
+        <mesh
+          ref={boxMeshRef}
+          rotation={[rotationX, THREE.MathUtils.degToRad(45), 0]}
+          position={[x, y, z]}
+          scale={[1, 1, 1]}
+          geometry={new THREE.BoxGeometry(1, 1, 1)}
+        >
+          <meshStandardMaterial color={"blue"} />
+        </mesh>
+
+        <mesh position={[0, 0, 0]} scale={[1, 1, 1]}>
+          <boxGeometry />
+          <meshStandardMaterial color={"red"} />
+        </mesh>
+        <Box position={[-1, -1, -1]}>
+          <meshStandardMaterial color={"green"} />
+        </Box>
+      </group>
     </>
   );
 };
