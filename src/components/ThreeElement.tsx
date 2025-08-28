b36a7c0ad3a1f0cf47d255507620c2b82e5efdab
unknown <thffhahs03@gmail.com>
2025-08-29 01:13:19 +0900

    mesh 마무리

--- src/components/ThreeElement.tsx
+++ src/components/ThreeElement.tsx
@@ -1,11 +1,11 @@
 import { useFrame, useThree } from "@react-three/fiber";
 import { useControls } from "leva";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { Box } from "@react-three/drei";
 
 const ThreeElement = () => {
   const boxMeshRef = useRef<THREE.Mesh>(null);
+  const secondBoxMeshRef = useRef<THREE.Mesh>(null);
   // const { size, gl, scene } = useThree();
   const { rotationX, x, y, z } = useControls({
     rotationX: { value: 0, min: -360, max: 360, step: 0.3 },
@@ -13,6 +13,15 @@ const ThreeElement = () => {
     y: { value: 0, min: -10, max: 10, step: 0.1 },
     z: { value: 0, min: -10, max: 10, step: 0.1 },
   });
+  const { width, height, depth, widthSegment, heightSegment, depthSegment } =
+    useControls({
+      width: { value: 1, min: 0, max: 5, step: 0.1 },
+      height: { value: 1, min: 0, max: 5, step: 0.1 },
+      depth: { value: 1, min: 0, max: 5, step: 0.1 },
+      widthSegment: { value: 1, min: 1, max: 10, step: 1 },
+      heightSegment: { value: 1, min: 1, max: 10, step: 1 },
+      depthSegment: { value: 1, min: 1, max: 10, step: 1 },
+    });
   const { clock } = useThree();
   useFrame((state, delta) => {
     // 박스 회전
@@ -22,6 +31,11 @@ const ThreeElement = () => {
       boxMeshRef.current.position.z = Math.sin(clock.getElapsedTime()) * 3;
     }
   });
+  useEffect(() => {
+    if (boxMeshRef.current && secondBoxMeshRef.current) {
+      secondBoxMeshRef.current.geometry = boxMeshRef.current.geometry;
+    }
+  }, [boxMeshRef.current?.geometry]);
   return (
     <>
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
@@ -31,18 +45,23 @@ const ThreeElement = () => {
           rotation={[rotationX, THREE.MathUtils.degToRad(45), 0]}
           position={[x, y, z]}
           scale={[1, 1, 1]}
-          geometry={new THREE.BoxGeometry(1, 1, 1)}
         >
+          <boxGeometry
+            args={[
+              width,
+              height,
+              depth,
+              widthSegment,
+              heightSegment,
+              depthSegment,
+            ]}
+          />
           <meshStandardMaterial color={"blue"} />
         </mesh>
 
-        <mesh position={[0, 0, 0]} scale={[1, 1, 1]}>
-          <boxGeometry />
-          <meshStandardMaterial color={"red"} />
+        <mesh ref={secondBoxMeshRef} position={[0, 0, 0]} scale={[1, 1, 1]}>
+          <meshStandardMaterial wireframe />
         </mesh>
-        <Box position={[-1, -1, -1]}>
-          <meshStandardMaterial color={"green"} />
-        </Box>
       </group>
     </>
   );
